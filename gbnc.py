eb6b1d67e1f788db9ae7603dc13219612593982f
Bo Gao <bo.gao@sib.swiss>
2016-11-11 13:49:16 +0100

    Generate individuals & variant set gbnc.py can generate individuals now, gvs.py can generate pre-set variant set.

--- gbnc.py
+++ gbnc.py
@@ -10,16 +10,17 @@ from bson import ObjectId
 @click.command()
 @click.option('-db', '--dbname', default='arraymap', help='The name of the database, default is "arraymap"')
 @click.option('-src', '--collection_src', default='samples', help='The collection to read from, default is "samples"')
+@click.option('-dsti', '--collection_dst_individuals', default='individuals', help='The collection to write into, default is "individuals"')
 @click.option('-dstb', '--collection_dst_biosamples', default='biosamples', help='The collection to write into, default is "biosamples"')
 @click.option('-dstc', '--collection_dst_callsets', default='callsets', help='The collection to write into, default is "callsets"')
-@click.option('-d', '--demo', default=0, type=click.IntRange(0, 10000), help='Only process a limited number of entries')
+@click.option('-d', '--demo', default=100, type=click.IntRange(0, 10000), help='Only process a limited number of entries')
 @click.option('--dnw', is_flag=True, help='Do Not Write to the db')
 @click.option('-l', '--log',  type=click.File('w'), help='Output errors and warnings to a log file')
 @click.option('-s', '--status',  default='-exclude', help='Filter by STATUS value, param must be in format [+/-]keyword. "+" means to include, "-" means to exclude.')
-def cli(dbname, collection_src, collection_dst_biosamples, collection_dst_callsets, demo, dnw, log, status):
+def cli(dbname, collection_src, collection_dst_individuals, collection_dst_biosamples, collection_dst_callsets, demo, dnw, log, status):
     """
     This script checks through the given source db collection,
-    generate new BIOSAMPLEs and CALLSETs, and put them in the
+    generate new INDIVIDUALS, BIOSAMPLEs and CALLSETs, and put them in the
     destination db collection.
 
     NOTE: Please manually create the destination collection to
@@ -43,6 +44,11 @@ def cli(dbname, collection_src, collection_dst_biosamples, collection_dst_callse
     db = client[dbname]
     if collection_src not in db.collection_names():
         print(collection_src + ' does not exist')
+        sys.exit()
+    if collection_dst_individuals not in db.collection_names():
+        print(collection_dst_individuals + ' does not exist')   
+        print('You have to create it first with \"mongo ' + dbname +
+              ' --eval \'db.createCollection("'+collection_dst_individuals+'")\'\"')    
         sys.exit()
     if collection_dst_biosamples not in db.collection_names():
         print(collection_dst_biosamples + ' does not exist')
@@ -78,7 +84,8 @@ def cli(dbname, collection_src, collection_dst_biosamples, collection_dst_callse
         click.echo('The first char of --status must be "+" or "-".')
         sys.exit()
 
-    # BIOSAMPLE and CALLSET data are stored here
+    # INDIVIDUAL, BIOSAMPLE and CALLSET data are stored here
+    individuals = {}
     biosamples = {}
     callsets = {}
     variantset_id = 'AM_VS_HG18'
@@ -90,21 +97,25 @@ def cli(dbname, collection_src, collection_dst_biosamples, collection_dst_callse
     ######################
     no_samples = 0
     no_validSamples = 0
+    no_individuals =0
     no_biosamples = 0
     no_callsets = 0
 
-    ######################
+    ########################################
     # return how many samples are processed.
-    ######################
+    ########################################
     def show_counter(item):
         if item is not None:
             return str(no_samples+1)+' samples processed'
 
+    ########################################
+    # return an attribute of a sample.
+    ########################################
     def get_attribute(name, sample):
         try:
             val = sample[name]
         except KeyError:
-            val = ''
+            val = 'null'
             if log is not None:
                 click.echo('KeyError: '+str(sample['_id'])+' has no '+name, file=log)
         return val
@@ -126,7 +137,7 @@ def cli(dbname, collection_src, collection_dst_biosamples, collection_dst_callse
                 if matchObj:
                     click.echo(no_samples, file=log)
 
-            no_validSamples += 1
+            # generate ids
             callset_id = 'AM_CS_'+sample['UID']
             biosample_id = 'AM_BS_'+sample['UID']
             individual_id = 'PGIND_'+sample['UID']
@@ -136,7 +147,41 @@ def cli(dbname, collection_src, collection_dst_biosamples, collection_dst_callse
             ############################################
             #only samples with enough attributes are assumed to be valid, the threshold is set to 50 arbitrarily.
             if (len(sample) > 50):
-                # check and generate biosample
+                no_validSamples += 1
+
+
+
+
+
+                # generate an individual
+                if individual_id in individuals:
+                    # if same individual_id exists, report an error
+                    if log is not None:
+                        click.echo('Duplicate individual_id: '+individual_id, file=log)
+                else:
+                    species = {'id': 'http://purl.obolibrary.org/obo/NCBITaxon_9606',
+                                'term': 'Homo sapiens',
+                                'source_name': 'NCBITaxon',
+                                'source_version': 'null'}
+                    if get_attribute('SEX', sample) is 'male':
+                        sex_id = 'http://purl.obolibrary.org/obo/PATO_0020001'
+                    elif get_attribute('SEX', sample) is 'female':
+                        sex_id = 'http://purl.obolibrary.org/obo/PATO_0020002'
+                    else:
+                        sex_id = 'null'
+                    sex = {'id': sex_id, 'term': 'genotypic sex', 'source_name': 'PATO', 
+                            'source_version':'http://purl.obolibrary.org/obo/pato/releases/2016-05-22/pato.owl'}
+
+                    individuals[individual_id] = {'id': individual_id, 'name': 'null', 'description': get_attribute('DIAGNOSISTEXT', sample), 
+                                                    'characteristics': 'null', 'created': datetime.datetime.utcnow(), 'updated': datetime.datetime.utcnow(),
+                                                    'species': species, 'sex': sex}
+                    no_individuals +=1
+
+
+
+
+
+                # generate a biosample
                 if biosample_id in biosamples:
                     # if same biosample_id exists, report an error
                     if log is not None:
@@ -171,7 +216,7 @@ def cli(dbname, collection_src, collection_dst_biosamples, collection_dst_callse
                                                     'sex': get_attribute('SEX', sample),
                                                     'death': get_attribute('DEATH', sample),
                                                     'followup_months': get_attribute('FOLLOWUP', sample),
-                                                    'redirected_to': ''},
+                                                    'redirected_to': 'null'},
                                                 'characteristics': {
                                                     'diseases': [
                                                         {
@@ -198,6 +243,11 @@ def cli(dbname, collection_src, collection_dst_biosamples, collection_dst_callse
                     }
                     no_biosamples += 1
 
+
+
+
+
+
                 # check and generate callset
                 if callset_id in callsets:
                     # if same callset_id exists, report an error
@@ -217,6 +267,9 @@ def cli(dbname, collection_src, collection_dst_biosamples, collection_dst_callse
                     else:
                         break
 
+
+
+
     ######################
     # display statistics
     ######################
@@ -229,13 +282,16 @@ def cli(dbname, collection_src, collection_dst_biosamples, collection_dst_callse
     click.echo('*'*60)
     click.echo()
 
+
+
+
     ######################
     # write to the db
     ######################
     if not dnw:
 
         # Commond line prompt to confirm the overwriting of db
-        click.echo('New data will overwrite collections: '+collection_dst_biosamples + ' and ' + collection_dst_callsets + '.')
+        click.echo('New data will overwrite collections: '+ collection_dst_individuals +', '+collection_dst_biosamples + ' and ' + collection_dst_callsets + '.')
         while True:
             msg = input('Do you want to proceed? Please type y/n: ')
             if msg is 'n':
@@ -244,19 +300,27 @@ def cli(dbname, collection_src, collection_dst_biosamples, collection_dst_callse
             elif msg is 'y':
 
                 # writing db
+                db_individuals = db[collection_dst_individuals]
+                db_individuals.remove()
+                with click.progressbar(individuals.items(), label='Writing Database ' + collection_dst_individuals + ':\t',
+                                       fill_char=click.style('>', fg='green')) as bar:
+                    for k, v in bar:
+                        insert_id = db_individuals.insert(v)    
+
                 db_biosamples = db[collection_dst_biosamples]
                 db_biosamples.remove()
-                with click.progressbar(biosamples.items(), label='Writing Database:' + collection_dst_biosamples,
+                with click.progressbar(biosamples.items(), label='Writing Database ' + collection_dst_biosamples + ':\t',
                                        fill_char=click.style('>', fg='green')) as bar:
                     for k, v in bar:
                         insert_id = db_biosamples.insert(v)
 
                 db_callsets = db[collection_dst_callsets]
                 db_callsets.remove()
-                with click.progressbar(callsets.items(), label='Writing Database:' + collection_dst_callsets,
+                with click.progressbar(callsets.items(), label='Writing Database ' + collection_dst_callsets + ':\t',
                                        fill_char=click.style('>', fg='green')) as bar:
                     for k, v in bar:
                         insert_id = db_callsets.insert(v)
+
                 break
             else:
                 print('invalid input')
---
+++ gvs.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+
+
+client = MongoClient()
+db = client.arraymap.variantsets
+db.remove()
+
+variant_set = {'id': 'AM_VS_HG18', 
+				'dataset_id': 'arraymap',
+				'reference_set_id:': 'HG18',
+				}
+
+insert_id = db.insert(variant_set)
