abe8f79649128bef4559d9ce53771a057a746682
Bo Gao <bo.gao@sib.swiss>
2017-01-23 14:41:07 +0100

    minor changes

--- gvs.py
+++ gvs.py
@@ -7,7 +7,7 @@ db.remove()
 
 variant_set = {'id': 'AM_VS_HG18', 
 				'dataset_id': 'arraymap',
-				'reference_set_id:': 'HG18',
+				'reference_set_id': 'HG18',
 				}
 
 insert_id = db.insert(variant_set)
